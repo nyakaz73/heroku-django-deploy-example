8c5dfb5e2141fe40c48dad67bf469638c507ab9a
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-01-06 18:07:25 +0200

    Django app and Action Updates

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ let heroku = {
     'app_name': core.getInput('heroku_app_name'),
     'api_key': core.getInput('heroku_api_key'),
     'email_address': core.getInput('heroku_email_address'),
+    'workingDir': core.getInput('working_directory'),
 }
 
+let execOptions = {
+    cwd: heroku.workingDir,
+}
 const createNetrcFileForLogin = ({ email_address, api_key }) => {
     return execSync(`cat >~/.netrc << EOF
 machine api.heroku.com
@@ -33,10 +37,47 @@ login = () => {
 }
 
 
+deployToHeroku = ({ app_name }) => {
+    try {
+        console.log('***************************');
+        console.log('--Login--')
+        console.log('***************************');
+
+        const login = execSync('heroku container:login').toString();
+        console.log(login);
+
+        //Build and Push the Image to CR
+        console.log('***************************');
+        console.log('--Build and Push Image--')
+        console.log('***************************');
+        const build_push = execSync(`heroku container:push --app ${app_name} web`, execOptions).toString();
+        console.log(build_push)
+
+        //App Migration
+        console.log('***************************');
+        console.log('--App Migration--')
+        console.log('***************************');
+        const migrate = execSync('heroku run python manage.py migrate', execOptions).toString();
+        console.log(migrate);
+
+        //Releasing the app
+        console.log('***************************');
+        console.log('--Releasing the app--')
+        console.log('***************************');
+        const release = execSync(`heroku container:release --app ${app_name} web`, execOptions).toString();
+        console.log(release);
+
+
+    } catch (error) {
+        console.log(error.message);
+        core.setFailed(error.message);
+    }
+}
 
 
 //Calls
 login();
+deployToHeroku(heroku);
 
 
 //Set output
